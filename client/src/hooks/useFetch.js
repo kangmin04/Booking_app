058a5b58ba49726d60c787021aa953920c35d77b
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2025-10-20 12:51:15 +0000

    Usefetch Implement

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,10 @@
+import {useEffect, useState} from "react"
+
+const useFetch = (url) => {
+    const [data , setData] = useState([]) ; 
+    const [loading , setLoading] = useState(false) ; 
+    const [error , setError] = useState(false) ; 
+
+    useEffect(() => {
+
+    } , [url])
