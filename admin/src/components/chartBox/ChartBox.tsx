7a19a1da89be92725e81c8ddfa86610f4c1ed5c7
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2025-11-03 14:36:39 +0000

    Implement ChartBox with typescript props for reuseable code

--- admin/src/components/chartBox/ChartBox.tsx
+++ admin/src/components/chartBox/ChartBox.tsx
@@ -1,76 +1,88 @@
 import "./chartBox.scss"
 import { Link } from "react-router-dom"
-import { Line, LineChart , Tooltip } from 'recharts';
+import { Line, LineChart , Tooltip , ResponsiveContainer} from 'recharts';
 
-const ChartBox = () => {
-    const data = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
+type Props = {
+    color: string , 
+    icon : string , 
+    title : string , 
+    number : string , 
+    dataKey : string , 
+    percentage : number , 
+    chartData : object[]
+}
+const ChartBox = (props : Props) => {
+    // const data = [
+    //     {
+    //       name: 'Page A',
+    //       uv: 4000,
+    //       pv: 2400,
+    //       amt: 2400,
+    //     },
+    //     {
+    //       name: 'Page B',
+    //       uv: 3000,
+    //       pv: 1398,
+    //       amt: 2210,
+    //     },
+    //     {
+    //       name: 'Page C',
+    //       uv: 2000,
+    //       pv: 9800,
+    //       amt: 2290,
+    //     },
+    //     {
+    //       name: 'Page D',
+    //       uv: 2780,
+    //       pv: 3908,
+    //       amt: 2000,
+    //     },
+    //     {
+    //       name: 'Page E',
+    //       uv: 1890,
+    //       pv: 4800,
+    //       amt: 2181,
+    //     },
+    //     {
+    //       name: 'Page F',
+    //       uv: 2390,
+    //       pv: 3800,
+    //       amt: 2500,
+    //     },
+    //     {
+    //       name: 'Page G',
+    //       uv: 3490,
+    //       pv: 4300,
+    //       amt: 2100,
+    //     },
+    //   ];
     return (
         <div className="chartBox">
             <div className="boxInfo">
                 <div className="title">
-                    <img src="/user.svg" alt="" className="" />
-                    <span>Total Users</span>
+                    <img src={props.icon} alt="" className="" />
+                    <span>{props.title}</span>
                 </div>
-                <h1 className="number">11,280</h1>
+                <h1 className="number">{props.number}</h1>
                 <Link to='/' className="view">View All</Link>
             </div>
             <div className="chartInfo">
 
                 <div className="charts">
+                {/* <ResponsiveContainer width="100%" height={100}> */}
                 <LineChart
                     style={{ width: '100%', maxWidth: '300px', maxHeight: '100px', aspectRatio: 1.618 }}
                     responsive
-                    data={data}
+                    data={props.chartData}
+                   
                     >
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-                    <Tooltip />
+                    <Line type="monotone" dataKey={props.dataKey} stroke={props.color} strokeWidth={2}  dot={false} />
+                    <Tooltip contentStyle={{background: 'transparent', color: '#fff' , border : 'none'}} labelStyle={{display : 'none'}} position={{x : 50 , y : 50}}/>
                     </LineChart>
+                    {/* </ResponsiveContainer > */}
                 </div>
                 <div className="texts">
-                    <span className="percentage">45%</span>
+                    <span className="percentage" style={{color : (props.percentage > 0 ? 'mangogreen' : 'tomato')}}>{props.percentage}%</span>
                     <span className="duration">this month</span>
                 </div>
             
--- admin/src/pages/home/Home.tsx
+++ admin/src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import TopBox from '../../components/topBox/TopBox'
 import ChartBox from '../../components/chartBox/ChartBox'
 import './home.scss'
+import {chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser} from '../../data.ts'; 
 
 const Home = () => {
 
@@ -9,11 +10,11 @@ const Home = () => {
             <div className="box box1">
                 <TopBox/>
             </div>
-            <div className="box box2"><ChartBox/></div>
-            <div className="box box3"><ChartBox/></div>
+            <div className="box box2"><ChartBox {...chartBoxUser}/></div>
+            <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
             <div className="box box4">box4</div>
-            <div className="box box5"><ChartBox/></div>
-            <div className="box box6"><ChartBox/></div>
+            <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
+            <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
             <div className="box box7">Box 7</div>
             <div className="box box8">Box 8</div>
             <div className="box box9">Box 9</div>
