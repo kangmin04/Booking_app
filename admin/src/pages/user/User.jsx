532ea7a671e6fa83f2b166d128be244a9f26fd8e
kangmin040823 <146157505+kangmin04@users.noreply.github.com>
2025-11-14 13:11:00 +0000

    on Progress in single page view

--- admin/src/pages/user/User.jsx
+++ admin/src/pages/user/User.jsx
@@ -1,12 +1,16 @@
 import './user.scss' 
 import Single from '../single/Single'
 import { singleUser } from '../../data.js'
+import {  useLocation } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch.js'
 
 const User = () => {
-
+    const location = useLocation(); 
+    const userId = location.pathname.split('/')[2]; 
+    const {data} = useFetch(`api/users/${userId}`)
+    console.log(data); 
     return (
         <div className="">
-        <p>this is a single usesr page</p>
             <Single {...singleUser}/>
         </div>
         
